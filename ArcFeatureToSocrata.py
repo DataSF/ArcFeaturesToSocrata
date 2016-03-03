5ed55b6c534fc688b6dae04bf0c25b5f1db99bf6
j9heiser <janine.heiser@sfgov.org>
2016-03-03 00:17:03 +0000

    reorganized methods

--- ArcFeatureToSocrata.py
+++ ArcFeatureToSocrata.py
@@ -1,11 +1,10 @@
 
 # coding: utf-8
 
-# In[625]:
+# In[764]:
 
 import re
 import csv
-import numpy as np
 import inflection
 import re
 import datetime
@@ -20,11 +19,10 @@ import itertools
 import datetime
 import bson
 import json
-import pyproj
 import time 
 
 
-# In[675]:
+# In[775]:
 
 class ArcFeatureToSocrata:
     
@@ -32,8 +30,8 @@ class ArcFeatureToSocrata:
         self.inputdir = inputdir
         self.fieldConfigFile = fieldConfigFile
         #self.clientConfigFile = clientConfigFile
-        self.configItems = getConfigs( self.inputdir, self.fieldConfigFile)
-        self.client = connectToSocrata(inputdir + self.configItems['socrata_client_config_fname'])
+        self.configItems = self.getConfigs( self.inputdir, self.fieldConfigFile)
+        self.client = self.connectToSocrata(inputdir + self.configItems['socrata_client_config_fname'])
         self.field_documentation_field = self.configItems['field_documentation_field']
         self.feature_service_endpoint_field = self.configItems['feature_service_endpoint_field']
         self.layer_field = self.configItems['layer_field']
@@ -47,15 +45,15 @@ class ArcFeatureToSocrata:
         self.destination_attribute_field = self.configItems['destination_attribute_field']
         self.srid_projection = str(self.configItems['srid_projection'])
         self.dataset_base_url = self.configItems['dataset_base_url']
-        
+    
     def makeDataSets(self):
         #load the publishing tracker
-        schemaLayout, datasets = make_headers_and_rowobj(self.configItems['inputDataDir'] + self.configItems['pubtracker'])
+        schemaLayout, datasets = self.make_headers_and_rowobj(self.configItems['inputDataDir'] + self.configItems['pubtracker'])
         return datasets
     
     def getAttributesForDataSets(self):
         #load the attribute list
-        datasetSchema, allAttributes =  make_headers_and_rowobj(self.configItems['inputDataDir'] + self.configItems['attribute_list'])
+        datasetSchema, allAttributes =  self.make_headers_and_rowobj(self.configItems['inputDataDir'] + self.configItems['attribute_list'])
         return allAttributes
 
     def setupDataSetSchema(self, dataset, allAttributes):
@@ -64,7 +62,7 @@ class ArcFeatureToSocrata:
             query = 'f=json'
             urlbase = dataset[self.feature_service_endpoint_field]
             layer = dataset[self.layer_field]
-            argisFields = getArgisDataType(urlbase, layer, query)
+            argisFields = self.getArgisDataType(urlbase, layer, query)
             if argisFields:
                 dataset['category'] = dataset[self.category_field]
                 dataset['tags'] = dataset[self.tags_field].split(';')
@@ -72,16 +70,15 @@ class ArcFeatureToSocrata:
                 dataset['name'] = dataset[self.socrata_name_field]
                 #wkid is the well known identifier for the projection associated with the dataset
                 dataset['wkid'] = argisFields['extent']['spatialReference']['latestWkid']
-                argisFieldsList, dataset['description'], geometryType = parseDataSetDescription(argisFields, 'fields', 'description', 'geometryType')
+                argisFieldsList, dataset['description'], geometryType = self.parseDataSetDescription(argisFields, 'fields', 'description', 'geometryType')
                 #now get the columns from the attribute definition
                 fname_dataset = dataset[self.layer_name_field].lower()
                 filterkeys= self.agency_prefix +"."+ fname_dataset
                 attributes = filterDictListOnKeyVal(allAttributes, 'Feature Class', [filterkeys])
-                #datasetSchema, attributes =  make_headers_and_rowobj(inputdir +fname_dataset)
                 dataset['columns'] = self.makeColumns(attributes, argisFieldsList, geometryType)
         return dataset
     
-       
+
     def makeDataSetSchemaForSocrata(self, dataset, allAttributes):
         dataset = self.setupDataSetSchema(dataset, allAttributes)
         if 'columns' in dataset.keys() and 'name' in dataset.keys():
@@ -110,7 +107,7 @@ class ArcFeatureToSocrata:
                 #now add the Geom Column
             geomDict = {'name': 'Geom', 'SourceAttribute': 'geometry', 'fieldName': 'geom','dataTypeName': geometryType }
             columns.append( geomDict)
-            columns = mapEsriDataTypes(columns)
+            columns = self.mapEsriDataTypes(columns)
             return columns
         else:
             return False
@@ -217,11 +214,11 @@ class ArcFeatureToSocrata:
                     print "could not filter row"
                 dataRow = {}
                 try:
-                    dataRow = columnLookup( row, dataRow, argisSourceColumnDataTypes )
+                    dataRow = self.columnLookup( row, dataRow, argisSourceColumnDataTypes )
                 except:
                     print "could not find column in lookup"
                 try:
-                    dataRow['geom'] = formatGeodata(shape['geometry'])
+                    dataRow['geom'] = self.formatGeodata(shape['geometry'])
                 except:
                     print
                     print "no geom"
@@ -233,157 +230,137 @@ class ArcFeatureToSocrata:
             except: 
                 print "couldn't get attributes as json count"
         return datasetOut
-
-
-
-# In[676]:
-
-def columnLookup( row, dataRow, argisSourceColumnDataTypes ):
-    for key,val in row.iteritems():
-        columnLookup =  (item for item in argisSourceColumnDataTypes if item["SourceAttribute"] == key).next()
-        if columnLookup['dataTypeName'] == 'date':
-            if not( row[key] is None):
-                row[key] = datetime.datetime.fromtimestamp(row[key]/1000.0)
+    
+    @staticmethod
+    def columnLookup( row, dataRow, argisSourceColumnDataTypes ):
+        for key,val in row.iteritems():
+            columnLookup =  (item for item in argisSourceColumnDataTypes if item["SourceAttribute"] == key).next()
+            if columnLookup['dataTypeName'] == 'date':
+                if not( row[key] is None):
+                    row[key] = datetime.datetime.fromtimestamp(row[key]/1000.0)
+                    try:
+                        row[key] =   str(row[key].strftime("%m/%d/%Y"))
+                    except ValueError:
+                        row[key] = None
+                        dataRow[columnLookup['fieldName']]  = row[key]
+                        #dataRow[columnLookup['fieldName']] = json.dumps( row[key], default=date_handler)
+                else:
+                    dataRow[columnLookup['fieldName']] = None
+            elif columnLookup['dataTypeName'] == 'text':
                 try:
-                    row[key] =   str(row[key].strftime("%m/%d/%Y"))
-                except ValueError:
-                    row[key] = None
+                    dataRow[columnLookup['fieldName']] = inflection.titleize(row[key].lower())
+                except:
                     dataRow[columnLookup['fieldName']]  = row[key]
-                    #dataRow[columnLookup['fieldName']] = json.dumps( row[key], default=date_handler)
             else:
-                dataRow[columnLookup['fieldName']] = None
-        elif columnLookup['dataTypeName'] == 'text':
+                dataRow[columnLookup['fieldName']] = row[key]
+        return dataRow
+    
+    @staticmethod
+    def formatGeodata( geom):
+        if not(geom is None):
+            if geom['type'] == 'Point':
+                geom =[str(x) for x in geom['coordinates']]
+                geom = "(" + ",".join(geom) + ")"
+                return geom
+            else:
+                geom['type'] = self.mapEsriToSocrata(geom['type'])
+                if geom['type'] == 'Line':
+                    geom['coordinates'] = geom['coordinates'][0]
+                    geom['type'] = 'LineString'
+                return geom
+        return geom
+    
+    @staticmethod
+    def getConfigs( inputdir, fieldConfigFile):
+        fieldItems = 0
+        with open(inputdir + fieldConfigFile ,  'r') as stream:
             try:
-                dataRow[columnLookup['fieldName']] = inflection.titleize(row[key].lower())
-            except:
-                dataRow[columnLookup['fieldName']]  = row[key]
+                fieldItems = yaml.load(stream)
+            except yaml.YAMLError as exc:
+                print(exc)
+        return fieldItems
+    
+    @staticmethod
+    def connectToSocrata( clientConfigFile):
+        with open(clientConfigFile,  'r') as stream:
+            try:
+                client_items = yaml.load(stream)
+                client = Socrata(client_items['url'],  client_items['app_token'], username=client_items['username'], password=base64.b64decode(client_items['password']))
+                return client
+            except yaml.YAMLError as exc:
+                print(exc)
+        return 0
+    
+    @staticmethod
+    def make_headers_and_rowobj( fname, keysToKeep=None):
+        with open(fname, 'rb') as inf:
+            dictList = []
+            reader = csv.DictReader(inf)
+            for row in reader:
+                dictList.append(row)
+            schemaLayout = dictList[0].keys()
+            if(keysToKeep == None):
+                keysToKeep = schemaLayout
+            useful = filterDictList(dictList, keysToKeep) 
+            return schemaLayout,useful
+        
+    @staticmethod
+    def getArgisDataType( urlbase, layer, query):
+        url = urlbase +"/" + layer+ "/?" + query
+        response = requests.get(url)
+        if response.status_code == 200:
+            response_json = response.json()
+            if 'error' in response_json.keys():
+                return False
+            else:
+                return response_json
         else:
-            dataRow[columnLookup['fieldName']] = row[key]
-    return dataRow
-
-
-# In[677]:
-
-def formatGeodata(geom):
-    if not(geom is None):
-        if geom['type'] == 'Point':
-            geom =[str(x) for x in geom['coordinates']]
-            geom = "(" + ",".join(geom) + ")"
-            return geom
+            return False
+        
+    @staticmethod
+    def mapEsriToSocrata( val):
+        dataTypesDict = { 'esriFieldTypeDate': 'date', 'esriGeometryPoint':'location', 
+                          'esriGeometryPolygon':'polygon', 'esriFieldTypeString':'text', 
+                          'esriFieldTypeDouble':'number',  'esriGeometryPointLine':'Line' ,
+                          'esriFieldTypeOID': 'number', 'esriFieldTypeInteger': 'number', 
+                          'esriFieldTypeSmallInteger': 'number', 'esriGeometryPolyline': 'Line', 
+                          'MultiLineString': 'Line'}
+        dataTypesDictKeys = dataTypesDict.keys()
+        if val in dataTypesDictKeys:
+            return dataTypesDict[val]
         else:
-            geom['type'] = mapEsriToSocrata(geom['type'])
-            if geom['type'] == 'Line':
-                geom['coordinates'] = geom['coordinates'][0]
-                geom['type'] = 'LineString'
-            return geom
-    return geom
+            return val
+        
+    @classmethod
+    def mapEsriDataTypes(self, dictList):
+        return [{k: self.mapEsriToSocrata(v) for k, v in mydict.iteritems()} for mydict in dictList]
+    
+    @staticmethod
+    def parseDataSetDescription(argisFields, fieldColumn, descriptionColumn, geometryTypeColumn):
+        argisFieldsList = argisFields[fieldColumn]
+        description = argisFields[ descriptionColumn]
+        geometryType =  argisFields[ geometryTypeColumn]
+        return argisFieldsList, description, geometryType
 
 
-# In[678]:
+# In[776]:
 
-def getConfigs(inputdir, fieldConfigFile):
-    fieldItems = 0
-    with open(inputdir + fieldConfigFile ,  'r') as stream:
-        try:
-            fieldItems = yaml.load(stream)
-        except yaml.YAMLError as exc:
-            print(exc)
-    return fieldItems
-
-
-# In[679]:
-
-def connectToSocrata(clientConfigFile):
-    with open(clientConfigFile,  'r') as stream:
-        try:
-            client_items = yaml.load(stream)
-            client = Socrata(client_items['url'],  client_items['app_token'], username=client_items['username'], password=base64.b64decode(client_items['password']))
-            return client
-        except yaml.YAMLError as exc:
-            print(exc)
-    return 0
-
-
-# In[680]:
-
-def make_headers_and_rowobj(fname, keysToKeep=None):
-    with open(fname, 'rb') as inf:
-        dictList = []
-        reader = csv.DictReader(inf)
-        for row in reader:
-            dictList.append(row)
-        schemaLayout = dictList[0].keys()
-        if(keysToKeep == None):
-            keysToKeep = schemaLayout
-        useful = filterDictList(dictList, keysToKeep) 
-        return schemaLayout,useful,
-
-
-# In[681]:
-
-def filterDictList(dictList, keysToKeep):
+def filterDictList( dictList, keysToKeep):
     return  [ {key: x[key] for key in keysToKeep if key in x.keys() } for x in dictList]
 
 def filterDict(mydict, keysToKeep):
     mydictKeys = mydict.keys()
     return {key: mydict[key] for key in keysToKeep  if key in mydictKeys }
-
+    
 def filterDictListOnKeyVal(dictlist, key, valuelist):
     #filter list of dictionaries with matching values for a given key
     return [dictio for dictio in dictlist if dictio[key] in valuelist]
-
-
-
-# In[682]:
-
-def getArgisDataType(urlbase, layer, query):
-    url = urlbase +"/" + layer+ "/?" + query
-    response = requests.get(url)
-    if response.status_code == 200:
-        response_json = response.json()
-        if 'error' in response_json.keys():
-            return False
-        else:
-            return response_json
-    else:
-        return False
-
-
-# In[683]:
-
-def mapEsriToSocrata(val):
-    dataTypesDict = { 'esriFieldTypeDate': 'date', 'esriGeometryPoint':'location', 
-                      'esriGeometryPolygon':'polygon', 'esriFieldTypeString':'text', 
-                      'esriFieldTypeDouble':'number',  'esriGeometryPointLine':'Line' ,
-                      'esriFieldTypeOID': 'number', 'esriFieldTypeInteger': 'number', 
-                      'esriFieldTypeSmallInteger': 'number', 'esriGeometryPolyline': 'Line', 
-                      'MultiLineString': 'Line'}
-    dataTypesDictKeys = dataTypesDict.keys()
-    if val in dataTypesDictKeys:
-        return dataTypesDict[val]
-    else:
-        return val
-  
-def mapEsriDataTypes(dictList):
-    return [{k: mapEsriToSocrata(v) for k, v in mydict.iteritems()} for mydict in dictList]
-
-
-# In[684]:
 
 def date_handler(obj):
     return obj.isoformat() if hasattr(obj, 'isoformat') else obj
 
 
-# In[685]:
-
-def parseDataSetDescription(argisFields, fieldColumn, descriptionColumn, geometryTypeColumn):
-    argisFieldsList = argisFields[fieldColumn]
-    description = argisFields[ descriptionColumn]
-    geometryType =  argisFields[ geometryTypeColumn]
-    return argisFieldsList, description, geometryType
-
-
-# In[701]:
+# In[777]:
 
 def main():
     inputdir = '/home/ubuntu/workspace/mta_test/'
@@ -417,15 +394,15 @@ def main():
         finalOutputStatus.append(dataset)
 
 
-# In[702]:
+# In[778]:
 
 if __name__ == '__main__' and '__file__' in globals():
     main()
 
 
-# In[703]:
+# In[779]:
 
-#main()
+main()
 
 
 # In[ ]:
